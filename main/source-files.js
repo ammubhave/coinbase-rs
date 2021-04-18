4f351e8111eeb5b565d622c5a4f0dc8b8e69967c
ammubhave <ammubhave@users.noreply.github.com>
2021-04-18 23:08:02 +0000

    deploy: e8680ab4a4523b176f2d5e00724683c3ca5145dd

--- main/source-files.js
+++ main/source-files.js
@@ -1,4 +1,3 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["coinbase"] = {"name":"","files":["accounts.rs","client.rs","errors.rs","lib.rs","transactions.rs","users.rs"]};
-sourcesIndex["get_current_user"] = {"name":"","files":["get_current_user.rs"]};
 createSourceSidebar();
